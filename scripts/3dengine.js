23692edfe88330b45f3545bc7fff6f72daea475f
Javier Bonilla Mite <mentor37_@hotmail.com>
2019-12-06 03:31:09 -0500

    [upgrade] added content, bootstrap styles, and ico

--- scripts/3dengine.js
+++ scripts/3dengine.js
@@ -10,9 +10,7 @@ function main() {
 
 	// MAIN METHOD THAT GOING TO INITIATE CAMERA, SCENE AND THE FIRST ROTATING CUBE
 	const canvas = document.querySelector('#c');
-	renderer = new THREE.WebGLRenderer({
-			canvas
-		});
+	renderer = new THREE.WebGLRenderer({canvas});
 
 	const fov = 50;
 	const aspect = 2; // the canvas default
@@ -36,25 +34,6 @@ function main() {
 	scene.add(light);
 	scene.background = new THREE.Color(0xd2d2d2);
 
-	// {
-	// const mtlLoader = new MTLLoader();
-	// mtlLoader.load('./resources/javb92_wepage_threejs.mtl', (mtlParseResult) => {
-	// const objLoader = new OBJLoader2();
-	// const materials =  MtlObjBridge.addMaterialsFromMtlLoader(mtlParseResult);
-	// console.log(materials);
-	// for (let mats in materials) {
-	// console.log(mats.Material);
-	// }
-	// //materials.Material.side = THREE.DoubleSide;
-	// objLoader.addMaterials(materials);
-	// objLoader.load('./resources/javb92_wepage_threejs.obj', (root) => {
-	// scene.add(root);
-	// root.position.set(0, 0, 0);
-	// root.scale.set(1, 1, 1);
-	// });
-	// });
-	// }
-
 
 	var loader = new GLTFLoader();
 	loader.load('./gltf/javb92_wepage_threejs.gltf', function (gltf) {
@@ -69,19 +48,20 @@ function main() {
 		const height = canvas.clientHeight;
 		const needResize = canvas.width !== width || canvas.height !== height;
 		if (needResize) {
-			renderer.setSize(width, height, false);
+		  renderer.setSize(width, height, false);
 		}
 		return needResize;
 	}
+	
 	let ver1 = true;
 	//console.log(scene.children);
 	function render(time, time2) {
 		time *= 0.00001; // convert time to seconds
 		time2 *= 0.00001; // convert time to seconds
 		if (resizeRendererToDisplaySize(renderer)) {
-			const canvas = renderer.domElement;
-			camera.aspect = canvas.clientWidth / canvas.clientHeight;
-			camera.updateProjectionMatrix();
+		  const canvas = renderer.domElement;
+		  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+		  camera.updateProjectionMatrix();
 		}
 
 		for (let cont = 0; cont < scene.children.length; cont++) {
@@ -113,6 +93,7 @@ function main() {
 			}
 			//scene.children[cont].rotation.y = time;
 		}
+		
 		renderer.render(scene, camera);
 
 		requestAnimationFrame(render);
